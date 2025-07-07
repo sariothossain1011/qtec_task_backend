f8a5f175bf458b4fb2eebf4989cfbd7b34083830
Sariot Hossain <sariothossain1011@gmail.com>
2025-07-07 18:39:35 +0600

    mongoose updated

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
 const app = require("./app")
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-dotenv.config({path:'./config.env'});
+dotenv.config({ path: './config.env' });
 
 const port = 8080;
 
-mongoose.connect(process.env.DATABASE,{autoIndex:true}).then(()=>{
+mongoose.connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    autoIndex: true,
+}).then(() => {
     console.log('MongoDB database connection successfully')
-}).catch((error)=>{
-    console.log('MongoDB database connection fail',error)
+}).catch((error) => {
+    console.log('MongoDB database connection fail', error)
 })
 
 
 
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`Server is runing at http://localhost:${port}`)
 })
